6c4b92b1a3eae93a68d4536a75a63d705c6f7ee7
Desarrollo <repositorios@arisoft.com.mx>
2020-02-01 12:44:29 -0700

    crear

--- src/models/crear_orden.js
+++ src/models/crear_orden.js
@@ -108,7 +108,7 @@ crearModel.insert_comren_json = (folio_orden, articulos, callback) => {
     //console.log(articulos);
     //console.log(folio_orden);
     console.log("se empezara a recorrer el articulos");
-    db.open(connectionString, function(err) {
+    dbCOBOL.open(connectionString, function(err) {
         if (err) {
     
             console.log('SERVIDOR MACROPRO NO RESPONDE - VERIFIQUE QUE ESTE ENCENDIDO');
